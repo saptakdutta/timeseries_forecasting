cb8d5228d1c850689bd14b649c0acbf560df1495
Saptak Dutta <saptakdutta@cmail.carleton.ca>
2024-06-26 23:53:08 -0400

    gitignore

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-#%% Libs
+# %% Libs
 from statsforecast import StatsForecast
 from statsforecast.models import AutoARIMA
 from statsforecast.utils import AirPassengersDF
 import numpy as np
 import pandas as pd
 
-#%% Read in data
-#https://nixtlaverse.nixtla.io/statsforecast/docs/getting-started/getting_started_complete.html
-df = pd.read_csv("three_months_energy_usage_data.csv")
-df = df.rename(columns={"DateTime":"ds","EnergyUsage_MJ":"y"})
-df["unique_id"] = "A1"
-
-#%% Set parameters
-sf = StatsForecast(
-    models = [AutoARIMA(season_length = 24)],
-    freq = 'H'
+# %% Read in data
+# https://nixtlaverse.nixtla.io/statsforecast/docs/getting-started/getting_started_complete.html
+# df = pd.read_csv("three_months_energy_usage_data.csv").rename(columns={"DateTime": "ds", "EnergyUsage_MJ": "y"})
+df = pd.read_csv("pjm_hourly_est.csv").rename(
+    columns={"Datetime": "ds", "PJM_Load": "y"}
 )
+df["unique_id"] = "A1"
+df
 
-#%% predictions
+# %% Set parameters
+sf = StatsForecast(models=[AutoARIMA(season_length=24)], freq="H")
+
+# %% predictions
 sf.fit(df)
 predictions = sf.predict(h=96, level=[75])
 
-#%% Plot predictions
-predictions.plot(x='ds',y='AutoARIMA')
+# %% Plot predictions
+predictions.plot(x="ds", y="AutoARIMA")
 
-#%% Examine with sample data
-df[0:48].plot(x='ds',y='y')
+# %% Examine with sample data
+df[0:48].plot(x="ds", y="y")
 
 # %%
