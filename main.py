0c0546d85808f34c431479c0a506e6ef79701234
Saptak.Dutta <saptak.dutta@nrc-cnrc.gc.ca>
2024-06-27 12:45:41 -0400

    edits

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # %% Libs
 from statsforecast import StatsForecast
 from statsforecast.models import AutoARIMA
-from statsforecast.utils import AirPassengersDF
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # %% Read in data
 # https://nixtlaverse.nixtla.io/statsforecast/docs/getting-started/getting_started_complete.html
@@ -12,20 +12,23 @@ df = pd.read_csv("pjm_hourly_est.csv").rename(
     columns={"Datetime": "ds", "PJM_Load": "y"}
 )
 df["unique_id"] = "A1"
-df = df[0:32895]
+df = df.sort_values(by="ds", ascending=True)
 df
 
 # %% Set parameters
 sf = StatsForecast(models=[AutoARIMA(season_length=24)], freq="H")
 
+# split dataset into train and test
+train_df, test_df = train_test_split(df, test_size=0.1, shuffle=False, stratify=None)
+
 # %% predictions
-sf.fit(df)
-predictions = sf.predict(h=96, level=[75])
+sf.fit(train_df.tail(1000))
+predictions = sf.predict(h=100, level=[75])
 
 # %% Plot predictions
 predictions.plot(x="ds", y="AutoARIMA")
 
 # %% Examine with sample data
-df[0:152].plot(x="ds", y="y")
+test_df[0:100].plot(x="ds", y="y")
 
 # %%
