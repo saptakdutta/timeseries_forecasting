d9245bef47fd31df2f83fcb48f3d0c5450dc2e04
Saptak Dutta <saptakdutta@cmail.carleton.ca>
2024-06-26 22:58:16 -0400

    plotting changes

--- main.py
+++ main.py
@@ -11,16 +11,20 @@ df = pd.read_csv("three_months_energy_usage_data.csv")
 df = df.rename(columns={"DateTime":"ds","EnergyUsage_MJ":"y"})
 df["unique_id"] = "A1"
 
-# %%
+#%% Set parameters
 sf = StatsForecast(
     models = [AutoARIMA(season_length = 24)],
     freq = 'H'
 )
 
-#%%
+#%% predictions
 sf.fit(df)
-predictions = sf.predict(h=48, level=[90])
+predictions = sf.predict(h=96, level=[75])
 
-# %%
+#%% Plot predictions
 predictions.plot(x='ds',y='AutoARIMA')
+
+#%% Examine with sample data
+df[0:48].plot(x='ds',y='y')
+
 # %%
